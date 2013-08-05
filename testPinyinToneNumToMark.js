37517991aabace59d4771dd93a9cd0c48a06fd8c
Jason Lau <lauwtj@yahoo.com>
2013-08-05 21:06:41 +0800

    fix case of funny xx5

--- testPinyinToneNumToMark.js
+++ testPinyinToneNumToMark.js
@@ -4,3 +4,4 @@ console.log( testConvert.pinyinToneNumToMark('han4 zi4') );
 console.log( testConvert.pinyinToneNumToMark('shu1') );
 console.log( testConvert.pinyinToneNumToMark('lu:e3 shu3 bu4'));
 console.log( testConvert.pinyinToneNumToMark('r5'));
+console.log( testConvert.pinyinToneNumToMark('xx5'));
